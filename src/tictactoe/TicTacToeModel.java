8dbbe43042d27a3bece1ea5a8dec16b4a925b5a6
Justin Pak <justinpak13@gmail.com>
2024-03-13 14:25:35 -0400

    commit before tictactoe model function implementations

--- src/tictactoe/TicTacToeModel.java
+++ src/tictactoe/TicTacToeModel.java
@@ -4,7 +4,89 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class TicTacToeModel implements TicTacToe {
+  final int NUMBER_OF_SQUARES = 9;
+  Player[][] board;
+  Player currentTurn;
+  Player winner;
+
+  int numberOfSpacesAvailable;
   // add your implementation here
+
+  public TicTacToeModel(){
+    this.board = new Player[3][3];
+    this.currentTurn = Player.X;
+    this.winner = null;
+    this.numberOfSpacesAvailable = NUMBER_OF_SQUARES;
+  }
+
+  /**
+   * Execute a move in the position specified by the given row and column.
+   *
+   * @param r the row of the intended move
+   * @param c the column of the intended move
+   * @throws IllegalArgumentException if the space is occupied or the position is otherwise invalid
+   * @throws IllegalStateException if the game is over
+   */
+  //TODO NEED TO IMPLEMENT
+  public void move(int r, int c){
+  }
+
+  /**
+   * Get the current turn, i.e., the player who will mark on the next call to move().
+   *
+   * @return the {@link Player} whose turn it is
+   */
+  //TODO NEED TO IMPLEMENT
+  public Player getTurn(){
+
+  }
+
+  /**
+   * Return whether the game is over. The game is over when either the board is full, or
+   * one player has won.
+   *
+   * @return true if the game is over, false otherwise
+   */
+  //TODO NEED TO IMPLEMENT
+  public boolean isGameOver(){
+
+  }
+
+  /**
+   * Return the winner of the game, or {@code null} if there is no winner. If the game is not
+   * over, returns {@code null}.
+   *
+   * @return the winner, or null if there is no winner
+   */
+  //TODO NEED TO IMPLEMENT
+  public Player getWinner(){
+
+  }
+
+  /**
+   * Return the current game state, as a 2D array of Player. A {@code null} value in the grid
+   * indicates an empty position on the board.
+   *
+   * @return the current game board
+   */
+  //TODO NEED TO IMPLEMENT
+  public Player[][] getBoard(){
+
+  }
+
+  /**
+   * Return the current {@link Player} mark at a given row and column, or {@code null} if the
+   * position is empty.
+   *
+   * @param r the row
+   * @param c the column
+   * @return the player at the given position, or null if it's empty
+   */
+  //TODO NEED TO IMPLEMENT
+  public Player getMarkAt(int r, int c){
+
+  }
+
 
   @Override
   public String toString() {
